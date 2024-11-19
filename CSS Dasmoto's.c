c61fbd220765ca7c480e5da692ce6b303c750c1d
LawzaTierney <168890941+LawzaTierney@users.noreply.github.com>
2024-11-19 17:50:51 +1100

    Add files via upload

---
+++ CSS Dasmoto's.c
@@ -0,0 +1,40 @@
+body {
+    font-family:Helvetica;
+}
+
+h1.title {
+    font-size:: 400px;
+    font-family: Helvetica;
+    font-weight:bold;
+    color: khaki;
+    text-align: center;
+    background-image: url("https://content.codecademy.com/courses/freelance-1/unit-2/pattern.jpeg?_gl=1*1jr6wfs*_gcl_aw*R0NMLjE3MzA3MDI0NjkuRUFJYUlRb2JDaE1Jdkplb2lZbkNpUU1WZDR0TEJSMjloVEluRUFBWUFTQUFFZ0tZSFBEX0J3RQ..*_gcl_au*MTc3NjYwNzU5MS4xNzMwNzAyNDY2*_ga*MzUyODY3MTcwMS4xNzMwNzAyNDYz*_ga_3LRZM6TM9L*MTczMTI5OTczMi41LjEuMTczMTI5OTczNi41Ni4wLjA.");
+}
+
+h2.Brushes {
+    font-family: Helvetica;
+    font-size:32px;
+    font-weight:bold;
+    background-color:mediumspringgreen;
+    color:white;
+}
+
+h2.Frames {
+    font-family: Helvetica;
+    font-size: 32px;
+    color: white;
+    font-weight: bold;
+    background-color:lightcoral;
+}
+
+h2.Paint{
+    font-family: Helvetica;
+    font-size: 32px;
+    color: white;
+    font-weight: bold;
+    background-color: skyblue;
+}
+
+strong {
+    color: blue;
+}
